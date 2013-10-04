cafe5e0b1bf59d09826bee053b4a6ac7c422edfd
Micah Barrett <mbarrett02@risd.edu>
2013-10-04 17:41:37 -0400

    Mobile Nav
    added some mobile nav functionality

---
+++ assets/js/common.js
@@ -0,0 +1,45 @@
+
+			 $(document).ready(function() {
+			    $(".innovation-title, .happening-title" ).lettering();
+			    // $(".happening-title").lettering();
+			    // $(".creativity-title").lettering();
+
+
+			    $(window).scroll(function(){
+			    	if($(window).scrollTop()>750){
+			    		$(".innovation-title").addClass("revealed");
+			    		
+
+			    	}
+
+			    	if($(window).scrollTop()>4550){
+			    		// $(".creativity-title").css("opacity","0.2");
+			    		$(".creativity-title").addClass("clean");
+
+
+			    	}
+
+			    	if($(window).scrollTop()>100){
+			    		// $(".creativity-title").css("opacity","0.2");
+			    		$(".main-nav-container").addClass("mobile-hidden");
+			    	}
+
+			    	if($(window).scrollTop()<100){
+			    		// $(".creativity-title").css("opacity","0.2");
+			    		$(".main-nav-container").removeClass("mobile-hidden");
+			    	}
+
+			    });
+
+			  	 $(".mobile-logo").click(function(){
+			    	$(".main-nav-container").toggleClass("mobile-hidden");
+			    });
+
+			  });
+
+
+
+	   	
+    
+	
+
